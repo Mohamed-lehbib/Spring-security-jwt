7303462ae886cdde9c24577909c19b7857b0f276
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2023-12-30 23:01:16 +0000

    adding extractAllClaims

--- src/main/java/com/springsecurityjwt/config/JwtService.java
+++ src/main/java/com/springsecurityjwt/config/JwtService.java
@@ -1,11 +1,17 @@
 package com.springsecurityjwt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
     public String extractUsername(String token) {
         return null;
+    }
 
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigninKey()).build().parseClaimsJws(token).getBody();
     }
 }
