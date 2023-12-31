c402b8a872473e7545d525d59d6635ecf04fd15c
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2023-12-31 17:44:23 +0000

    adding the extractUsername

--- src/main/java/com/springsecurityjwt/config/JwtService.java
+++ src/main/java/com/springsecurityjwt/config/JwtService.java
@@ -14,7 +14,7 @@ public class JwtService {
 
     private static final String SECRET_KEY = "f3a796792d2bb49cc5bf3ddc836f4b04be26e8e0773f2c5a262583f7abaf343b";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
